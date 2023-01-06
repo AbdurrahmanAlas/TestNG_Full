3810780eb3d59e8f07475b9a241be28281115171
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2023-01-06 20:09:21 +0300

    nutella

--- src/test/java/tests/day14TestNGFramework/C02_DependsOnMethod.java
+++ src/test/java/tests/day14TestNGFramework/C02_DependsOnMethod.java
@@ -1,8 +1,28 @@
 package tests.day14TestNGFramework;
 
+import org.testng.annotations.Test;
+
 public class C02_DependsOnMethod {
 
 
+    @Test()
+    public  void ilkTest(){
+
+        System.out.println("illk teest calisti");
+
+    }
+    @Test(dependsOnMethods = "ilkTest")
+    public  void ikinciTest(){
+
+        System.out.println("2 teest calisti");
+
+    }
+    @Test (dependsOnMethods = "ikinciTest ")
+    public  void ucuncuTest(){
+
+        System.out.println("3 teest calisti");
+
+    }
 
 
 
